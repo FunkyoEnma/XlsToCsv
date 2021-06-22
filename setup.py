210cb8256145329eda6e4e054eb4eaae6cc07ea3
Funkyo Enma <jlcruz.199812@gmail.com>
2021-06-22 08:12:57 -0500

    Setup created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ExcelToCsv',
+    version='1.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Funkyo Enma',
+    author_email='enmafunkyo@gmail.com',
+    description='Convert xls files to csv'
+)
